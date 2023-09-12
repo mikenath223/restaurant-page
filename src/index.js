e249ee62213d7fc9ae1e33d6a0cb93d774461833
luwat <oluwatobioreniyi@gmail.com>
2023-09-12 18:21:31 +0100

    remove commented codes

--- src/index.js
+++ src/index.js
@@ -2,14 +2,7 @@ import './style.css';
 import { nav, navBox, home, menu, contact } from './navbar';
 
 const element = document.querySelector('#content');
-// const nav = document.createElement('div');
-// const navWrapper = document.createElement('ul');
-// const navLink = document.createElement('a');
 
-// navLink.innerText = "Home";
-// navLink.setAttribute('href', '#')
-// navWrapper.appendChild(navLink);
-// nav.appendChild(navWrapper);
 navBox.appendChild(home);
 navBox.appendChild(menu);
 navBox.appendChild(contact);
