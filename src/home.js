18b2a041e791374ff7f0ae24e37341d363eb5246
luwat <oluwatobioreniyi@gmail.com>
2023-09-13 09:53:36 +0100

    create home.js

---
+++ src/home.js
@@ -0,0 +1,5 @@
+const Home = () => ({
+    _div:  this.div,
+
+
+})();
--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import { nav, navBox, home, menu, contact } from './navbar';
 const restaurantPage = () => {
 
     const element = document.querySelector('#content');
+    const main = document.createElement('main');
     
     navBox.appendChild(home);
     navBox.appendChild(menu);
     navBox.appendChild(contact);
     nav.appendChild(navBox);
-    
-    return element.appendChild(nav);
-
+    element.appendChild(nav)
+    return element.appendChild(main)
 }
 
 export default restaurantPage();
