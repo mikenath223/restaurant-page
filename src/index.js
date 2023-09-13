9c8a5addecc8a408ecdc5d26c51fda902c4c89a3
luwat <oluwatobioreniyi@gmail.com>
2023-09-13 09:01:52 +0100

    change PageLoad to Navbar

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import './style.css';
 import { nav, navBox, home, menu, contact } from './navbar';
+const restaurantPage = () => {
 
-const element = document.querySelector('#content');
+    const element = document.querySelector('#content');
+    
+    navBox.appendChild(home);
+    navBox.appendChild(menu);
+    navBox.appendChild(contact);
+    nav.appendChild(navBox);
+    
+    return element.appendChild(nav);
 
-navBox.appendChild(home);
-navBox.appendChild(menu);
-navBox.appendChild(contact);
-nav.appendChild(navBox);
+}
 
-element.appendChild(nav);
-
+export default restaurantPage();
