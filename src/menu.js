504939604314a79bab04ca12c455a8da78dc2391
luwat <oluwatobioreniyi@gmail.com>
2023-09-15 08:12:25 +0100

    fix setAttribute bug

--- src/menu.js
+++ src/menu.js
@@ -39,17 +39,25 @@ const Menu = (() => {
         _menus.map(menu => {
             const card = document.createElement('div');
             card.setAttribute('class', 'card');
+            
             const menuDesc = document.createElement('div');
-            menuDesc.setAttribute('menu-desc');
+            menuDesc.setAttribute('class', 'menu-desc');
+            
             const img = document.createElement('img');
             img.src = _menus.image;
 
             const heading = document.createElement('div');
             heading.textContent = _menus.heading;
+            
             const desc = document.createElement('p');
             desc.textContent = _menus.description;
 
-            
+            menuDesc.appendChild(heading);
+            menuDesc.appendChild(desc);
+
+            card.appendChild(img);
+            card.appendChild(menuDesc);
+
             return _menuBox.appendChild(card);
         });
 
