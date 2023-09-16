7cf6102ce82c1ade63b29c64073bd27ed052bd1a
luwat <oluwatobioreniyi@gmail.com>
2023-09-16 14:14:44 +0100

    use tabbed browsing

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import headline from './home';
 import menuPage from './menu';
 import contactPage from './contact';
 
+
 const restaurantPage = () => {
 
     const element = document.querySelector('#content');
@@ -14,10 +15,42 @@ const restaurantPage = () => {
     navBox.appendChild(contact);
     nav.appendChild(navBox);
     main.appendChild(headline);
-    main.appendChild(menuPage);
-    main.appendChild(contactPage);
     element.appendChild(nav);
-    return element.appendChild(main);
+    
+    
+    element.appendChild(main);
+    const btns = document.querySelectorAll('.nav-btn');
+
+    return btns.forEach(btn => {
+        btn.addEventListener('click', e => {
+            if (e.target.textContent === 'Home') {
+                if (main.contains(menuPage)) {
+                    return main.replaceChild(headline, menuPage)
+                } else if (main.contains(contactPage)) {
+                    return main.replaceChild(headline, contactPage)
+                } else {
+                    return main.appendChild(headline);
+                }
+
+            } else if (e.target.textContent === 'Menu') {
+                if (main.contains(headline)) {
+                    return main.replaceChild(menuPage, headline)
+                } else if (main.contains(contactPage)) {
+                    return main.replaceChild(menuPage, contactPage)
+                } else {
+                    return main.appendChild(menuPage);
+                }
+            } else if (e.target.textContent === 'Contact') {
+                if (main.contains(headline)) {
+                    return main.replaceChild(contactPage, headline)
+                } else if (main.contains(menuPage)) {
+                    return main.replaceChild(contactPage, menuPage)
+                } else {
+                    return main.appendChild(contactPage);
+                }
+            }
+        })
+    })
 }
 
 export default restaurantPage();
