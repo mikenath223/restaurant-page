712645b6db0b65c97d8f615a0d1addd63b5d990c
luwat <oluwatobioreniyi@gmail.com>
2023-09-18 12:24:21 +0100

    style nav

--- src/navbar.js
+++ src/navbar.js
@@ -1,26 +1,33 @@
 class Navbar {
+
     nav = document.createElement('nav');
     navBox = document.createElement('ul');
     navList = document.createElement('li');
     navLink = document.createElement('a');
+
     constructor(name, clas) {
         this.name = name;
         this.clas = clas
     }
 
     createLink() {
+
         this.navLink.textContent = this.name;
         this.navLink.setAttribute('href', `#${this.clas}`);
         this.navLink.setAttribute('class', this.clas);
         this.navList.setAttribute('class', 'nav-btn')
         this.navList.appendChild(this.navLink);
+
         return this.navBox.appendChild(this.navList);
+
     }
 }
+
 const nav = new Navbar().nav;
 const navBox = new Navbar().navBox;
 
 const home = new Navbar('Home', 'home').createLink();
 const menu = new Navbar('Menu', 'menu').createLink();
 const contact = new Navbar('Contact', 'contact').createLink();
+
 export {nav, navBox, home, menu, contact};
