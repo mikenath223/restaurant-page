6426bc777d06613c2446f4eebdbd8c18c358ba2a
luwat <oluwatobioreniyi@gmail.com>
2023-09-19 12:56:49 +0100

    working on loaders

--- webpack.config.js
+++ webpack.config.js
@@ -7,11 +7,27 @@ module.exports = {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    devtool: 'inline-source-map',
     module: {
         rules: [
             {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(gif|png|avif|jpe?g)$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: "[name][ext]",
+                    publicPath: "assets/images/",
+                    outputPath: "assets/images/",
+                },
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    'html-loader'
+                ]
             },
         ],
     },
